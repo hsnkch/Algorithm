1237f509f83f8cb88951c1834e24ce5eabe2a328
hsnkch <chsick@naver.com>
2023-02-12 17:20:04 +0900

    230212

--- A8911_230207.py
+++ A8911_230207.py
@@ -1,13 +1,46 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
-east = []
-west = []
-south = []
-north = []
 
-test = []
+T = int(input())    # test 케이스 수
 
-for i in n:
-    test[i] = input()
-    
+
+
+for i in range(T):
+    order = list(input().rstrip())  # 명령 리스트로 받기
+    x, y = [0], [0] # 초기좌표
+    direction = 0   # 나머지 0, 1, 2, 3 = 북, 동, 남, 서
+     
+    for j in order:  
+        if j == 'F':    # 전진일때 x,y 좌표 x,y 리스트에 추가
+            if direction % 4 == 0:
+                x.append(x[len(x)-1])
+                y.append(y[len(y)-1]+1)
+            if direction % 4 == 1:
+                x.append(x[len(x)-1]+1)
+                y.append(y[len(y)-1])
+            if direction % 4 == 2:
+                x.append(x[len(x)-1])
+                y.append(y[len(y)-1]-1)
+            if direction % 4 == 3:
+                x.append(x[len(x)-1]-1)
+                y.append(y[len(y)-1])
+        if j == 'B':    # 후진일때 x,y 좌표 x,y 리스트에 추가
+            if direction % 4 == 0:
+                x.append(x[len(x)-1])
+                y.append(y[len(y)-1]-1)
+            if direction % 4 == 1:
+                x.append(x[len(x)-1]-1)
+                y.append(y[len(y)-1])
+            if direction % 4 == 2:
+                x.append(x[len(x)-1])
+                y.append(y[len(y)-1]+1)
+            if direction % 4 == 3:
+                x.append(x[len(x)-1]+1)
+                y.append(y[len(y)-1])
+        if j == 'L':    # 좌짝으로 돌기
+            direction += 3
+        if j == 'R':    # 우짝으로 돌기
+            direction += 1
+    width = abs(min(x))+abs(max(x))
+    height = abs(min(y))+abs(max(y))    
+    print(width*height)
