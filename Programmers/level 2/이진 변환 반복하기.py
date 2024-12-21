71bd48b4977ce2706dfc002e39423aca375d35c1
Your Name <you@example.com>
2024-12-21 17:28:14 +0900

    241221

---
+++ Programmers/level 2/이진 변환 반복하기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    cnt = 0
+    zero_count = 0
+
+    while s != "1":
+        zero_count += s.count('0')
+        s = s.replace('0', '')
+        
+        s = bin(len(s))[2:]
+        
+        cnt += 1
+
+    return [cnt, zero_count]
+
+
+
+
